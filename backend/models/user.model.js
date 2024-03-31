2ba5c27666683a9afd5249716b03350d692de2b6
Houssein Droubi <housseinalialdroubi@gmail.com>
2024-03-31 16:54:46 +0300

    implement generating token for users

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const user_schema = new mongoose.Schema({
   username: {
@@ -11,6 +15,20 @@ const user_schema = new mongoose.Schema({
     required: true,
   },
 });
+
+user_schema.methods.generateToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.PRIVATE_KEY,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  return token;
+};
 
 const User = mongoose.model("users", user_schema);
 export default User;
